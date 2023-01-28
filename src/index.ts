3dbe6cf2c30237316efda838ce17d63dea6eefff
Guilherme <guifelicio7@gmail.com>
2023-01-27 21:52:22 -0300

    chore: add tsc-alias to build and tsconfig-paths to  dev

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
+import Tree from "@/tree/index";
+
 class Person {
   sayMayName() {
     return "Guilherme";
+  }
+
+  pathTest() {
+    const tree = new Tree();
+    tree.whoIAm();
   }
 }
 
---
+++ src/tree/index.ts
@@ -0,0 +1,7 @@
+class Tree {
+  whoIAm() {
+    console.log("i am a tree");
+  }
+}
+
+export default Tree;
